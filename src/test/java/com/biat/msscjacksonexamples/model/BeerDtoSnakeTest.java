2d545126e7a72fec30c605c7703600ec990cad1b
aymenbraiek <aymen.braiek@esprit.tn>
2020-04-10 17:05:32 +0100

    Jackson Property Naming Strategies -closes #2

---
+++ src/test/java/com/biat/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,26 @@
+package com.biat.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@JsonTest
+@ActiveProfiles("snake")
+public class BeerDtoSnakeTest extends  BeerTest {
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto beerDto=getDto();
+        String jsonString=objectMapper.writeValueAsString(beerDto);
+        System.out.println(jsonString);
+    }
+
+
+}
